2180a6b4392241e0e5cd2194b010472d5790e790
kobie-hendrix <oasamuel62@gmail.com>
2023-02-22 08:34:39 -0800

    alx-low_level_programing

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,9 +8,9 @@
  *
  * Return:1 for lowercase character. 0 for the rest.
  */
-int _islower (int c);
+int _islower(int c);
 {
-	if (c >= 97 && c<== 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (i);
 	}
