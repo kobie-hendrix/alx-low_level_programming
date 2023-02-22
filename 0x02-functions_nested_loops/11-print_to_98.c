494d36ec9ac85818905a7d24eb7b2342abc26d36
kobie-hendrix <oasamuel62@gmail.com>
2023-02-22 15:36:39 -0800

    alx

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,10 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d", n);
+			_putchar("%d", n);
 			if (n != 98)
 			{
-				printf(", ");
+				_putchar(", ");
 			}
 			n++;
 		}
@@ -23,17 +23,17 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d", n);
+			_putchar("%d", n);
 			if (n != 98)
 			{
-				printf(", ");
+				_putchar(", ");
 			}
 			n--;
 		}
 	}
 	else
 	{
-		printf("98");
+		_putchar("98");
 	}
-	printf("\n");
+	_putchar("\n");
 }
