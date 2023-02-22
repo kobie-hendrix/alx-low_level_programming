719cf16e2339a6b563c366c488d12d69a5ddd400
kobie-hendrix <oasamuel62@gmail.com>
2023-02-22 08:17:50 -0800

    alx-low_level_programing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 Prints the alphabet 10 times.
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for(j = 1; i <=10; i++)
+	for(j = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
 		{
